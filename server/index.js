2bdcf8b9e17b494544718dc60df61f07fa3e5026
Tamerlan12345 <centr-cool@mail.ru>
2025-10-17 14:11:48 +0500

    Revert "feat(tts): Add listen to course button and functionality"

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 // --- CONFIGURATION AND SETUP ---
 const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '..', 'config.env') });
 
 const express = require('express');
 const cors = require('cors');
